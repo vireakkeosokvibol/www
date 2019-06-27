7abc138a29f4eb6a040979fbd558bf023c85b9dc
VIREAK KEOSOKVIBOL <vireakkeosokvibol@bondom.co>
2019-06-27 14:02:07 +0700

    Initial ionic project.

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { HomePage } from './home.page';
+import { FooterComponent } from '../layouts/footer/footer.component';
+import { HeaderComponent } from '../layouts/header/header.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ])
+  ],
+  declarations: [FooterComponent, HomePage, HeaderComponent]
+})
+export class HomePageModule {}
