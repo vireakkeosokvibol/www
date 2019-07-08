ba1fae6da736bf48efa9fdb1c3e26ff439d93fa6
VIREAK KEOSOKVIBOL <vireakkeosokvibol@bondom.co>
2019-07-08 23:00:05 +0700

    Add global config for app.

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { APPURL } from '../../../config.app.json'
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+
+  private APPURL: string = APPURL;
 
   constructor() { }
 
