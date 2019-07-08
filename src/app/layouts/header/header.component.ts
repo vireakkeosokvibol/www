2380cfb5eb90f6e9442ca5b57eeced3c5f0412e2
VIREAK KEOSOKVIBOL <vireakkeosokvibol@bondom.co>
2019-07-08 23:12:42 +0700

    Fix typo.

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { APPURL } from '../../../config.app.json'
+import { APPURL } from '../../../config.app.json';
 
 @Component({
   selector: 'app-header',
